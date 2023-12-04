d931f21682b23ea7e2331114f6892fd83e62206b
LuanVo <vothanhluan19102001@gmail.com>
2023-12-04 17:44:45 +0700

    Resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
 
 import EmployeeContextProvider from './contexts/employeeContext';
 import TechnicalContextProvider from './contexts/technicalContext';
 import ComponentsContextProvider from './contexts/componentsContext';
-import AddEmployee from './pages/employee/addEmployee';
-import ButtonCommon from './components/ButtonCommon';
+
+import Employees from './pages/employee/employees';
 
 function App() {
   return (
-    <EmployeeContextProvider>
-      <TechnicalContextProvider>
-        <ComponentsContextProvider>
-        <div>
-          Sidebar
-          <br/>
-
-        </div>
-        <div>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/employee/create" element={<AddEmployee />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
-        </ComponentsContextProvider>
-      </TechnicalContextProvider>
-    </EmployeeContextProvider>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#3F51B5',
+          borderRadius: 2,
+        },
+      }}
+    >
+      <EmployeeContextProvider>
+        <TechnicalContextProvider>
+          <ComponentsContextProvider>
+            <div>
+              Sidebar 
+            </div>
+            <div>
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/employee" element={<Employees />} />
+                </Routes>
+              </BrowserRouter>
+            </div>
+          </ComponentsContextProvider>
+        </TechnicalContextProvider>
+      </EmployeeContextProvider>
+    </ConfigProvider>
   );
 }
 
