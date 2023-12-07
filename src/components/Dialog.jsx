34dccf1de3495a5d48777b2e04c7904d7615e2d2
tientvgcd201862 <tientvgcd201862@fpt.edu.vn>
2023-12-07 14:13:34 +0700

    fix button modal

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Modal } from 'antd';
+import ButtonCommon from './buttons/ButtonCommon';
 
 const Dialog = ({ isOpen, handleOk, handleCancel, title, message }) => {
   return (
     <Modal
       title={title}
       visible={isOpen}
-      onOk={handleOk}
-      onCancel={handleCancel}
+      onCancel={handleCancel} 
+      footer={null} 
     >
       <p>{message}</p>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <ButtonCommon buttonType="cancel" handleOnClick={handleCancel} />
+        <div style={{ marginLeft: '10px' }}>
+          <ButtonCommon buttonType="save" handleOnClick={handleOk} />
+        </div>
+      </div>
     </Modal>
   );
 };
 
 export default Dialog;
-
-
