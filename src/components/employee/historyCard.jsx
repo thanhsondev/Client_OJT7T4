160e2575831021921234834d1a507c11c7e9d219
LuanVo <vothanhluan19102001@gmail.com>
2023-12-11 11:39:12 +0700

    fix employee history

--- src/components/employee/historyCard.jsx
+++ src/components/employee/historyCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { Card, Tag } from 'antd';
-import ButtonCommon from '../buttons/ButtonCommon'
+import ButtonCommon from '../buttons/ButtonCommon';
+import { format } from 'date-fns';
 
 const HistoryCard = (history) => {
     const navigate = useNavigate();
@@ -26,7 +27,9 @@ const HistoryCard = (history) => {
         <p>Description: {employeeHistory.description}</p>
         <br />
         <p>Join: {employeeHistory.joinDate}</p>
-        <p>Out: {employeeHistory.outDate ? employeeHistory.outDate : "On working"}</p>
+        <p>Out: {employeeHistory.outDate ? employeeHistory.joinDate : "On working"}</p>
+        <p>Join: {format(new Date(employeeHistory.joinDate), 'yyyy-MM-dd')}</p>
+        <p>Out: {employeeHistory.outDate ? format(new Date(employeeHistory.joinDate), 'yyyy-MM-dd') : "On working"}</p>
         <ButtonCommon buttonType="details" handleOnClick={() => handleDetails(employeeHistory.projectId._id)} />
     </Card>
     </>
