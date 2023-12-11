7b1a5e8e4f3202bba649f3c61bc2e3ef1304efe6
thanhsondev <sondvtgcd201619@fpt.edu.vn>
2023-12-11 11:49:31 +0700

    Create 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,29 @@ import { Layout } from './components/layout/Layout';
 import { Dashboard } from './pages/dashboard/Dashboard';
 import Employees from './pages/employee/employees';
 import EmployeeDetails from './pages/employee/employeeDetails';
-
+import PageNotFound from "./pages/404notfound/PageNotFound";
 function App() {
    return (
-      <LayoutContextProvider>
-         <ComponentsContextProvider>
-            <TechnicalContextProvider>
-               <EmployeeContextProvider>
-                  <Routes>
-                     <Route path="/" element={<Layout />}>
-                        <Route path="dashboard" element={<Dashboard />} />
-                        <Route path="employee">
-                           <Route index element={<Employees />} />
-                           <Route path=":employeeId" element={<EmployeeDetails />} />
-                        </Route>
-                     </Route>
-                  </Routes>
-               </EmployeeContextProvider>
-            </TechnicalContextProvider>
-         </ComponentsContextProvider>
-      </LayoutContextProvider>
+       <LayoutContextProvider>
+          <ComponentsContextProvider>
+             <TechnicalContextProvider>
+                <EmployeeContextProvider>
+                   <Routes>
+                      <Route path="/" element={<Layout />}>
+                         <Route path="dashboard" element={<Dashboard />} />
+                         <Route path="employee">
+                            <Route index element={<Employees />} />
+                            <Route path=":employeeId" element={<EmployeeDetails />} />
+                         </Route>
+                      </Route>
+                   </Routes>
+                </EmployeeContextProvider>
+                <Routes>
+                   <Route path="/doesnt-exist" element={<PageNotFound/>} />
+                </Routes>
+             </TechnicalContextProvider>
+          </ComponentsContextProvider>
+       </LayoutContextProvider>
    );
 }
 
