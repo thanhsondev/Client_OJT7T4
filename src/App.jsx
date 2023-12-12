edfcd9b834a3173e3c24acbbd38f5d9e9fdb318a
LuanVo <vothanhluan19102001@gmail.com>
2023-12-12 17:03:02 +0700

    resolve conflict from Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,13 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
 
-import EmployeeContextProvider from './contexts/employeeContext';
-import TechnicalContextProvider from './contexts/technicalContext';
-import ComponentsContextProvider from './contexts/componentsContext';
-import LayoutContextProvider from './contexts/LayoutContext';
-import RecordContextProvider from './contexts/recordlogContext';
-
-import { Layout } from './components/layout/Layout';
-import { Dashboard } from './pages/dashboard/Dashboard';
-import LogPage from './pages/log/logPage';
-import Employees from './pages/employee/employees';
-import EmployeeDetails from './pages/employee/employeeDetails';
-import Projects from './pages/project/projects';
-import ProjectDetails from './pages/project/projectDetails';
+import AuthContextProvider from './contexts/authContext';
+import Router from './Router/routes';
 
 function App() {
    return (
-      <LayoutContextProvider>
-         <ComponentsContextProvider>
-            <TechnicalContextProvider>
-               <EmployeeContextProvider>
-                  <RecordContextProvider>
-                     <Routes>
-                        <Route path="/" element={<Layout />}>
-                           <Route path="dashboard" element={<Dashboard />} />
-                           <Route path="log" element={<LogPage />} />
-                           <Route path="employee">
-                              <Route index element={<Employees />} />
-                              <Route path=":employeeId" element={<EmployeeDetails />} />
-                           </Route>
-                           <Route path="project">
-                              <Route index element={<Projects />} />
-                              <Route path=":projectId" element={<ProjectDetails />} />
-                           </Route>
-                        </Route>
-                     </Routes>
-                  </RecordContextProvider>
-               </EmployeeContextProvider>
-            </TechnicalContextProvider>
-         </ComponentsContextProvider>
-      </LayoutContextProvider>
+      <AuthContextProvider>
+         <Router/>
+      </AuthContextProvider>
    );
 }
 
