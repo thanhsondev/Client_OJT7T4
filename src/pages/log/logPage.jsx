a7ded15c9dfa997fbd50f47604f5fcf7b3e1fe0f
LuanVo <vothanhluan19102001@gmail.com>
2023-12-13 23:58:04 +0700

    filter record log

--- src/pages/log/logPage.jsx
+++ src/pages/log/logPage.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { RecordContext } from '../../contexts/recordlogContext';
-import { Spin, Timeline } from 'antd';
-const { format } = require('date-fns');
+import { Spin, Timeline, Radio, DatePicker } from 'antd';
+import { format } from 'date-fns';
+import moment from 'moment';
 
 const LogPage = () => {
     const {
@@ -9,13 +10,44 @@ const LogPage = () => {
         getRecords,
     } = useContext(RecordContext);
 
+    const [value, setValue] = useState('');
+    const [date, setDate] = useState(null);
+    const [filteredRecords, setFilteredRecords] = useState([]);
+
+    const onChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    const onDateChange = (date, dateString) => {
+        const formattedDates = moment(dateString, 'YYYY-MM-DDTHH:mm:ss.SSSZ').toISOString();
+        setDate(formattedDates);
+    };
+
     useEffect(() => {
-        getRecords();
-    }, []);
+        const filterRecords = async () => {
+            await getRecords();
 
-    let body = null;
+            let filtered = records;
 
-    const recordTimelines = records.map((red) => ({
+            if (value) {
+                filtered = filtered.filter((red) =>
+                    red.object.toLowerCase().includes(value.toLowerCase())
+                );
+            }
+
+            if (date) {
+                filtered = filtered.filter((red) => {
+                    return format(new Date(red.createAt), 'yyyy-MM-dd') === format(new Date(date), 'yyyy-MM-dd')
+                });
+            }
+
+            setFilteredRecords(filtered);
+        };
+
+        filterRecords();
+    }, [value, date, getRecords, records]);
+
+    const recordTimelines = filteredRecords.map((red) => ({
         label: format(new Date(red.createAt), 'yyyy-MM-dd HH:mm:ss'),
         children: red.record,
         color: {
@@ -26,25 +58,51 @@ const LogPage = () => {
         }[red.type] || 'gray',
     }));
 
+    let body = null;
+
     if (isLoading) {
         body = (
             <div className="spinner">
                 <Spin size="large" />
             </div>
-        )
+        );
     } else {
         body = (
             <>
-                <Timeline reverse={true} mode={"left"} items={recordTimelines} />
+                <Timeline reverse={true} mode={'left'} items={recordTimelines} />
             </>
-        )
+        );
     }
+
+    const filterObject = (
+        <>
+            <Radio.Group onChange={onChange} value={value}>
+                <Radio value={''}>All</Radio>
+                <Radio value={'Employee'}>Employee</Radio>
+                <Radio value={'Project'}>Project</Radio>
+                <Radio value={'User'}>User</Radio>
+            </Radio.Group>
+        </>
+    );
+
+    const filterDate = (
+        <>
+            <DatePicker onChange={onDateChange} />
+        </>
+    );
+
     return (
         <>
             <h1>Records</h1>
-            {body}
+            <div style={{ display: 'flex' }}>
+                <div style={{ display: 'block' }}>
+                {filterObject}
+                {filterDate}
+                </div>
+                {body}
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default LogPage
+export default LogPage;
