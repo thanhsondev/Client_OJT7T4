c8c09c3f60dd78cc1e1f820a1eeede3095bc412d
thanhsondev <132500171+thanhsondev@users.noreply.github.com>
2023-12-15 17:38:34 +0700

    Update PageNotFound.jsx

--- src/pages/404notfound/PageNotFound.jsx
+++ src/pages/404notfound/PageNotFound.jsx
@@ -1,37 +1,15 @@
 import React, { useState, useEffect } from "react";
 
-import { Link } from 'react-router-dom';
+
 
 const PageNotFound = () => {
-    const [imageSrc, setImageSrc] = useState("");
 
-    useEffect(() => {
-        const fetchImage = async () => {
-            try {
-                const response = await fetch(
-                    "https://res.cloudinary.com/do0ououdk/image/upload/f_auto,q_auto/bppb2nollbzds0l8gezf"
-                );
-                if (response.ok) {
-                    const blob = await response.blob();
-                    const imageURL = URL.createObjectURL(blob);
-                    setImageSrc(imageURL);
-                } else {
-                    console.error("Failed to fetch the image");
-                }
-            } catch (error) {
-                console.error("Error fetching the image:", error);
-            }
-        };
-
-        fetchImage();
-    }, []);
 
     return (
         <>
             <div className="cont-404">
-                {imageSrc && (
-                    <img src={imageSrc} alt="404" />
-                )}
+
+                <img src={'https://res.cloudinary.com/do0ououdk/image/upload/f_auto,q_auto/bppb2nollbzds0l8gezf'} alt="404" />
 
                 <button onClick={() => { window.location.href = '/'; }}>Back to Dashboard</button>
 
